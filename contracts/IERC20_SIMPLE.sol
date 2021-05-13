0e8d364f898028ceb83b1f5503546409dc914dd7
Greg Kapka <greg@oraclize.it>
2021-05-13 18:02:05 +0100

    feat(IERC20_SIMPLE): <- adds `burn` fxn sig to that

--- contracts/IERC20_SIMPLE.sol
+++ contracts/IERC20_SIMPLE.sol
@@ -2,5 +2,6 @@
 pragma solidity >=0.7.0 < 0.8.0;
 
 interface IERC20_SIMPLE {
-    function mint(address _to, uint256 _amount) external returns (bool success);
+    function mint(address _to, uint256 _amount) external;
+    function burn(address _from, uint256 _amount) external;
 }
